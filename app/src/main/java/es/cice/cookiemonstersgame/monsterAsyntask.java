e3e3d996d8642efdbf553d37e852f06a26f5fbf6
Joel <joelfernandezreviejo@gmail.com>
2017-01-31 13:53:41 +0100

    Random

--- app/src/main/java/es/cice/cookiemonstersgame/monsterAsyntask.java
+++ app/src/main/java/es/cice/cookiemonstersgame/monsterAsyntask.java
@@ -2,6 +2,8 @@ package es.cice.cookiemonstersgame;
 
 import android.os.AsyncTask;
 
+import java.util.Random;
+
 /**
  * Created by cice on 31/1/17.
  */
@@ -11,6 +13,10 @@ public class monsterAsyntask extends AsyncTask<Void,Integer, Void > {
     private tarroGalletas tarroG;
     @Override
     protected Void doInBackground(Void... params) {
+        tarroG=tarroGalletas.getInstance();
+
+        //tiempo aleatorio menor que 5 seg
+        Random rnd = new Random();
 
         tarroG.comeGalleta();
         return null;
